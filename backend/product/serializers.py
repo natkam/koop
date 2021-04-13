f5934c967051d3f84df824ff703e7f7381df35ef
Jerzy Spendel <jspendel@gmail.com>
2021-04-13 17:03:18 +0200

    black

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -6,4 +6,4 @@ from product.models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = "__all__"
--- backend/product/views.py
+++ backend/product/views.py
@@ -5,5 +5,5 @@ from product.serializers import ProductSerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
-    queryset = Product.objects.select_related('week')
+    queryset = Product.objects.select_related("week")
     serializer_class = ProductSerializer
