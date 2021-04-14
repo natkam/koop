6731d2b6c761333889f3d5d1698b2d8c792d4575
Jerzy Spendel <jspendel@gmail.com>
2021-04-14 22:34:25 +0200

    Use react-bootstrap

--- frontend/src/features/products/productTable.js
+++ frontend/src/features/products/productTable.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const ProductTable = () => {
-  const products = useSelector((state) => state.products.products);
-  return (
-    <>
-      <table>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td>{product.name}</td>
+
+const ProductTable = props => {
+    const products = useSelector(state => state.products.products);
+    return <>
+        <Table striped bordered hover variant="dark" size="sm">
+            <thead>
+            <tr>
+                <th>Nazwa</th>
+                <th>Zamawiania ilość</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+            </thead>
+            <tbody>
+            {
+                products.map(product => <tr key={product.id}>
+                    <td>{product.name}</td>
+                    <td>
+                        <Form>
+                            <Form.Group controlId="amount">
+                                <Form.Control type="number" placeholder="Amount"/>
+                            </Form.Group>
+                        </Form>
+                    </td>
+                </tr>)
+            }
+            </tbody>
+        </Table>
+        <Button>Złóż zamówienie</Button>
     </>
-  );
-};
-
-export { ProductTable };
+}
+export { ProductTable }
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
         test: /\.js$/,
         use: ["babel-loader"],
       },
