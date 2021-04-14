c47ac214f298693b95c19a660d77565f96690807
Jerzy Spendel <jspendel@gmail.com>
2021-04-14 22:43:26 +0200

    Update package-lock.json

--- frontend/src/features/products/productTable.js
+++ frontend/src/features/products/productTable.js
@@ -1,37 +1,38 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import Table from 'react-bootstrap/Table';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-const ProductTable = props => {
-    const products = useSelector(state => state.products.products);
-    return <>
-        <Table striped bordered hover variant="dark" size="sm">
-            <thead>
-            <tr>
-                <th>Nazwa</th>
-                <th>Zamawiania ilość</th>
+const ProductTable = () => {
+  const products = useSelector((state) => state.products.products);
+  return (
+    <>
+      <Table striped bordered hover variant="dark" size="sm">
+        <thead>
+          <tr>
+            <th>Nazwa</th>
+            <th>Zamawiania ilość</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product) => (
+            <tr key={product.id}>
+              <td>{product.name}</td>
+              <td>
+                <Form>
+                  <Form.Group controlId="amount">
+                    <Form.Control type="number" placeholder="Amount" />
+                  </Form.Group>
+                </Form>
+              </td>
             </tr>
-            </thead>
-            <tbody>
-            {
-                products.map(product => <tr key={product.id}>
-                    <td>{product.name}</td>
-                    <td>
-                        <Form>
-                            <Form.Group controlId="amount">
-                                <Form.Control type="number" placeholder="Amount"/>
-                            </Form.Group>
-                        </Form>
-                    </td>
-                </tr>)
-            }
-            </tbody>
-        </Table>
-        <Button>Złóż zamówienie</Button>
+          ))}
+        </tbody>
+      </Table>
+      <Button>Złóż zamówienie</Button>
     </>
-}
-export { ProductTable }
+  );
+};
+export { ProductTable };
