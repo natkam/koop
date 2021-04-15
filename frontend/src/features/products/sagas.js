5c02747297af2b106fce6bab1f82e934e76cca78
Jerzy Spendel <jspendel@gmail.com>
2021-04-15 22:32:02 +0200

    Add miragejs

--- frontend/src/features/products/sagas.js
+++ frontend/src/features/products/sagas.js
@@ -2,6 +2,11 @@ import { put } from "redux-saga/effects";
 import { setProducts } from "./productSlice";
 
 function* fetchProducts() {
+  fetch("http://localhost:8000/products/").then((response) => {
+    response.json().then((data) => {
+      console.log(data);
+    });
+  });
   const response = yield fetch("http://localhost:8000/products/");
   const products = yield response.json();
 
