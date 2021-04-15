30c13642c880dba525b57e82b92be5f3d4ba53f5
natkam <natka_m@o2.pl>
2021-04-15 20:07:05 +0200

    Register admin models

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from product.models import Product
+from product.models import PersonalOrder, Pickup, Product, Week
+
+
+@admin.register(PersonalOrder)
+class PersonalOrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Pickup)
+class PickupAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Week)
+class WeekAdmin(admin.ModelAdmin):
     pass
