65438cbe7487dae168a6cf05da5eceef71c8bfda
natkam <natka_m@o2.pl>
2021-04-15 22:07:42 +0200

    Remove django-dotenv from dev dependencies
    Docker didn't want to cooperate with it, the way I used it.

--- backend/koop/settings.py
+++ backend/koop/settings.py
@@ -12,9 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-import dotenv
-
-dotenv.read_dotenv(dotenv="../")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
