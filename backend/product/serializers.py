b36690a8e430d71a2e71bfbe988afa207a380077
natkam <natka_m@o2.pl>
2021-05-03 21:46:55 +0200

    Refactor order update: delete and create product orders

--- backend/product/serializers.py
+++ backend/product/serializers.py
@@ -1,6 +1,5 @@
-from typing import Any, Dict, List, TYPE_CHECKING
+from typing import Any, TYPE_CHECKING
 
-from django.db.models import QuerySet
 from product.models import PersonalOrder, Pickup, Product, ProductPersonalOrder, Week
 from rest_framework import serializers
 
@@ -47,55 +46,7 @@ class ProductSerializer(ModelSerializer):
         ]
 
 
-class ProductPersonalOrderListSerializer(ListSerializer):
-    def update(
-        self,
-        instance: "QuerySet[ProductPersonalOrder]",
-        validated_data: List[Dict[str, Any]],
-    ) -> List[ProductPersonalOrder]:
-        """Updates ordered products in a personal order.
-
-        When a product is added to a personal order, a new ProductPersonalOrder
-        (M2M relation) is created. When amount of an ordered product is changed, the
-        existing ProductPersonalOrder object is updated. When amount of an ordered
-        product is set to a falsey value (`null` or 0), the ProductPersonalOrder
-        relation is deleted from the database.
-
-        The implementation is based on the example in the docs:
-        https://www.django-rest-framework.org/api-guide/serializers/#customizing-multiple-update
-
-        Args:
-            instance: Queryset of ProductPersonalOrders, i.e. the products ordered by
-                the user this week (before the update).
-            validated_data: List of dicts representing the ProductPersonalOrders after
-                the update; PPOs where the ordered amount is not null nor 0.
-
-        Returns:
-            List of ProductPersonalOrders representing the products ordered by the user.
-        """
-        # Perform creations and updates.
-        ordered = []
-        ordered_ids = []
-        for ppo_data in validated_data:
-            ppo, created = instance.update_or_create(
-                personal_order=ppo_data["personal_order"],
-                product=ppo_data["product"],
-                defaults=ppo_data,
-            )
-            ordered.append(ppo)
-            ordered_ids.append(ppo.id)
-        # TODO(Nat): Make sure only the ordered amount data can be altered by the user.
-
-        # Perform deletions.
-        for ppo in instance:
-            if ppo.id not in ordered_ids:
-                ppo.delete()
-
-        return ordered
-
-
 class ProductPersonalOrderSerializer(ModelSerializer):
     class Meta:
         model = ProductPersonalOrder
-        list_serializer_class = ProductPersonalOrderListSerializer
-        fields = "__all__"
+        fields = ["id", "product", "personal_order", "amount"]
--- backend/product/views.py
+++ backend/product/views.py
@@ -8,7 +8,7 @@ from django.db.models import (
 )
 from product.models import PersonalOrder, Product, ProductPersonalOrder, Week
 from product.serializers import ProductPersonalOrderSerializer, ProductSerializer
-from rest_framework import generics, status
+from rest_framework import generics, mixins, status
 from rest_framework.exceptions import NotFound
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -20,16 +20,19 @@ else:
     GenericAPIView = generics.GenericAPIView
 
 
-class OrderView(GenericAPIView):
-    http_method_names = ["get", "put", "head", "options", "trace"]
+class OrderView(GenericAPIView, mixins.ListModelMixin, mixins.UpdateModelMixin):
+    """View of the personal order of the user, allows updating the order."""
 
     def get(self, request: Request, week_id: int) -> Response:
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
+        """Lists all current week products with the amounts ordered by the user."""
+        return self.list(request, week_id)
 
     def put(self, request: Request, week_id: int) -> Response:
-        """Updates the user's order, or creates a new one if it does not exist."""
+        """Updates the user's order, or creates a new one if it does not exist.
+
+        Personal order update is performed by deleting all related product
+        personal orders and creating new ones based on the request data.
+        """
         # TODO(Nat): Only allow `put` for authenticated users.
         if isinstance(request.user, AnonymousUser):
             return Response(data="nope", status=status.HTTP_401_UNAUTHORIZED)
@@ -37,24 +40,22 @@ class OrderView(GenericAPIView):
         personal_order, _ = PersonalOrder.objects.get_or_create(
             week_id=week_id, user=request.user
         )
-        queryset = personal_order.product_personal_orders.all()
+        personal_order.product_personal_orders.all().delete()
         request_data = cast(List[Dict[str, Any]], request.data)
         ordered_data = [
             {
                 "personal_order": personal_order.id,
-                "product": data_item["id"],
-                "amount": data_item["ordered_amount"],
+                "product": data_item.get("id"),
+                "amount": data_item.get("ordered_amount"),
             }
             for data_item in request_data
-            if data_item["ordered_amount"]
+            if data_item.get("ordered_amount")
         ]
-        serializer = self.get_serializer(queryset, data=ordered_data, many=True)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-
+        serializer = self.get_serializer(data=ordered_data, many=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
         # TODO(Nat): Handle invalid data properly, add tests.
-        return Response("Wrong data. What a pity!", status=status.HTTP_400_BAD_REQUEST)
+        return Response(status=status.HTTP_201_CREATED)
 
     def get_queryset(self) -> "QuerySet[Product]":
         week_id: int = self._get_week_id()
